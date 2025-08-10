1f4377fa2873fe9bbbd8a41a81b16583aba3d8a8
Marko Tintor <marko.tintor.18@singimail.rs>
2025-08-10 18:09:26 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import "./style.css";
 import Products from "./Components/Products";
 import { useState } from "react";
-
-function clickOnButton() {
-  console.log("works");
-}
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  let [name, setName] = useState("hello world");
   let [tax, setTax] = useState("");
-  return (
-    <>
-      <Products tax={tax} />
 
-      <button onClick={clickOnButton}>{name}</button>
-      <input type="text" onInput={(e) => setName(e.target.value)}></input>
-      <input
-        type="text"
-        placeholder="unesite taxu"
-        onInput={(e) => setTax(e.target.value)}
-      ></input>
-    </>
+  return (
+    <div className="container">
+      <div className="mt-2">
+        <input
+          className="form-control"
+          type="text"
+          placeholder="Unesite novu taxu"
+          onInput={(e) => setTax(e.target.value)}
+        ></input>
+      </div>
+      <Products tax={tax} />
+    </div>
   );
 }
 
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -10,6 +10,7 @@ function Products(props) {
   });
   let [newProductName, setProductName] = useState();
   let [newProductPrice, setNewProductPrice] = useState();
+  let [newMessage, setNewMessage] = useState();
 
   function addProduct() {
     console.log(newProductName, newProductPrice);
@@ -21,29 +22,60 @@ function Products(props) {
       ...newProduct,
     }));
   }
-  return (
-    <>
-      <button onClick={() => setProducts({})}>delete</button>
-      {Object.entries(products).map(([phone, price]) => (
-        <p>
-          {phone}, price: ${price}, with tax ${calculateTax(price, props.tax)}
-        </p>
-      ))}
 
-      <div>
-        <input
-          onInput={(e) => setProductName(e.target.value)}
-          type="text"
-          placeholder="unesite ime proizvoda"
-        ></input>
-        <input
-          onInput={(e) => setNewProductPrice(e.target.value)}
-          type="number"
-          placeholder="unesite cenu proizvoda"
-        ></input>
-        <button onClick={addProduct}>add new product</button>
+  function checkApp(e) {
+    const searchTerm = e.currentTarget.value.toLowerCase();
+    const keys = Object.keys(products);
+    for (let key in keys) {
+      let keyName = keys[key].toLowerCase();
+      if (searchTerm === keyName) {
+        setNewMessage("nasli smo proizvod");
+        break;
+      } else {
+        setNewMessage("nismo nasli proizvod");
+      }
+    }
+  }
+  return (
+    <div>
+      <button onClick={() => setProducts({})}>delete</button>
+      <div className="d-flex justify-content-center">
+        {Object.entries(products).map(([phone, price]) => (
+          <div className="m-2">
+            <h4>{phone}</h4>
+            <p>${calculateTax(price, props.tax)}</p>
+          </div>
+        ))}
       </div>
-    </>
+      <p>{newMessage}</p>
+      <input
+        type="text"
+        placeholder="Unesite ime proizvoda za proveru"
+        onInput={checkApp}
+      ></input>
+      <div>
+        <h3>Dodavanje Proizvoda</h3>
+        <div className="mt-2">
+          <input
+            className="form-control"
+            onInput={(e) => setProductName(e.target.value)}
+            type="text"
+            placeholder="unesite ime proizvoda"
+          ></input>
+        </div>
+        <div className="mt-2">
+          <input
+            className="form-control"
+            onInput={(e) => setNewProductPrice(e.target.value)}
+            type="number"
+            placeholder="unesite cenu proizvoda"
+          ></input>
+        </div>
+        <button className="btn btn-primary mt-2" onClick={addProduct}>
+          add new product
+        </button>
+      </div>
+    </div>
   );
 }
 
